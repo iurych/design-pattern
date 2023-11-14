0c6b092de228d9976d002bc5e33466731cb183c3
iurych <iury.vibe@gmail.com>
2023-11-14 20:47:54 -0300

    chore: add gym and checkIn tables

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
-
+const prisma = new PrismaClient()
 // ORM - Object Relational Mapper
-
+prisma.user.create({
+  data: {
+    email: 'iury.vibe@gmail.com',
+    name: 'iury chiganer',
+  },
+})
