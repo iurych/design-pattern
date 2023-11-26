9ab51ebb868714d2aac8609ce76adb83e6fd146f
iurych <iury.vibe@gmail.com>
2023-11-26 12:10:09 -0300

    feat: uptodate

--- src/use-cases/register.test.ts
+++ src/use-cases/register.test.ts
@@ -1,5 +1,21 @@
-import { expect, describe, it } from 'vitest'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('check if it is working', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Casse', () => {
+  it('Should hash the password after register', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUserCase = new RegisterUseCase(prismaUsersRepository)
+
+    const { user } = await registerUserCase.execute({
+      name: 'John Doe',
+      email: 'johndoe5@example.com',
+      password: '123456',
+    })
+
+    const isPWDHashed = await compare('123456', user.password_hash)
+
+    expect(isPWDHashed).resolves.toBe(true)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { UserRepository } from '@/repositories/users-repository'
+import type { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
@@ -8,10 +9,18 @@ interface RegisterUser {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRespository: UserRepository) {}
 
-  async execute({ email, name, password }: RegisterUser) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUser): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const checkUniqueEmail = await this.usersRespository.findByEmail(email)
@@ -21,10 +30,14 @@ export class RegisterUseCase {
     }
 
     // inversão de dependência
-    await this.usersRespository.create({
+    const user = await this.usersRespository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
