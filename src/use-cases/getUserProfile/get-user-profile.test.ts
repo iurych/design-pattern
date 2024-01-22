549506b81dac884d750bd9567d3eb3f3ccf92228
iurych <iury.vibe@gmail.com>
2024-01-22 10:40:56 -0300

    fix test but from get-uset-profile

--- src/use-cases/getUserProfile/get-user-profile.test.ts
+++ src/use-cases/getUserProfile/get-user-profile.test.ts
@@ -1,49 +1,37 @@
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { hash } from 'bcryptjs'
 import { beforeEach, describe, expect, it } from 'vitest'
-import { AuthenticateUseCase } from '../authenticate/authenticate'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
 
 let usersRepository: InMemoryUsersRepository
-let sut: AuthenticateUseCase
+let sut: GetUserProfileUseCase
 
 describe('Get User profile use case', () => {
   beforeEach(() => {
     usersRepository = new InMemoryUsersRepository()
-    sut = new AuthenticateUseCase(usersRepository)
+    sut = new GetUserProfileUseCase(usersRepository)
   })
 
   it('it should be possible to get a user profile', async () => {
-    
-    const { user } = await sut.execute({
-      email: 'johndoe@example.com',
-      password: '123456',
-    })
-
-    expect(user.id).toEqual(expect.any(String))
-  })
-
-  it('it should not be able to get a user profile with wrong id', async () => {
-    expect(async () => {
-      await sut.execute({
-        email: 'johnd@example.com',
-        password: '1234',
-      })
-    }).rejects.toBeInstanceOf(InvalidCredentialsError)
-  })
-
-  it('it should be able to authenticate with wrong password', async () => {
-    await usersRepository.create({
+    const createdUser = await usersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
     })
 
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('John Doe')
+  })
+
+  it('it should not be able to get a user profile with wrong id', async () => {
     expect(async () => {
       await sut.execute({
-        email: 'johnd@example.com',
-        password: '1234234',
+        userId: 'fake-id',
       })
-    }).rejects.toBeInstanceOf(InvalidCredentialsError)
+    }).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
