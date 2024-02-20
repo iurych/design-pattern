4f4867151ea5ed5219d1656f0f78bdf281fabbbf
iurych <iury.vibe@gmail.com>
2024-02-20 15:43:57 -0300

    feat: add profileController

--- src/http/controller/profile-controller.ts
+++ src/http/controller/profile-controller.ts
@@ -1,3 +1,4 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export const profileUserController = async (
@@ -6,7 +7,12 @@ export const profileUserController = async (
 ) => {
   await request.jwtVerify()
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user,
+  })
 }
