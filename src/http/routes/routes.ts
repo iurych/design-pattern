4548e5a53c8ed610ecaa5ca2375ced34c7795c51
iurych <iury.vibe@gmail.com>
2024-02-20 20:59:24 -0300

    feat: add profileController and fix some issues

--- src/http/routes/routes.ts
+++ src/http/routes/routes.ts
@@ -1,8 +1,10 @@
 import { registerUserController } from '@/http/controller/register-controller'
 import { FastifyInstance } from 'fastify'
 import { authenticateUserController } from '../controller/authenticate-controller'
+import { profileUserController } from '../controller/profile-controller'
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post('/users', registerUserController)
   app.post('/sessions', authenticateUserController)
+  app.get('/profile', profileUserController)
 }
