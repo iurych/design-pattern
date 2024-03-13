c51e83f36047ea52c2793dd39a9a546f36ea410f
iurych <iury.vibe@gmail.com>
2024-03-13 10:33:00 -0300

    feat: add database_url to test configuration

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,42 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const generateDatabaseURL = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a valid DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+const prisma = new PrismaClient()
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       // eslint-disable-next-line @typescript-eslint/no-empty-function
-      teardown() {
-        console.log('Teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
