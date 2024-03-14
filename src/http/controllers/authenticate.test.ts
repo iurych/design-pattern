73a305f99befa4f66f12118e50a802e3c837a09e
iurych <iury.vibe@gmail.com>
2024-03-14 18:59:37 -0300

    chore: add register, profile and auth controllers e2e tests

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -11,13 +11,20 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
-    const response = await request(app.server).post('/users').send({
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johnexample@example.com',
       password: '123456',
     })
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johnexample@example.com',
+      password: '123456',
+    })
 
-    expect(response.statusCode).toEqual(201)
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
