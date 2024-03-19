d0f60c80601d07b9e6ded30add4b826c11a9c696
iurych <iury.vibe@gmail.com>
2024-03-18 23:25:22 -0300

    feat: add refresh token test

--- src/http/routes/users-routes.ts
+++ src/http/routes/users-routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '../controllers/middlewares/virifyjwt'
 import { authenticateUserController } from '../controllers/users/authenticate'
 import { profileUserController } from '../controllers/users/profile'
+import { refreshController } from '../controllers/users/refresh'
 
 export const usersRoutes = async (app: FastifyInstance) => {
   app.post('/users', registerUserController)
   app.post('/sessions', authenticateUserController)
+  app.patch('/token/refresh', refreshController)
 
   /** Authtenticated */
   app.get('/profile', { onRequest: [verifyJWT] }, profileUserController)
